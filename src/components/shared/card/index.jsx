f6b8b6dc5ab6c8107dc9a3e3b7246c1f0947ed3b
Saurabh-stack <saurabh007ind@gmail.com>
2023-09-06 14:09:18 +0530

    Accept Pill component and rest attributes

--- src/components/shared/card/index.jsx
+++ src/components/shared/card/index.jsx
@@ -1,14 +1,31 @@
 import { classNames } from "../../../utilities/classNames";
 
-const Card = ({ noHover, title, className, children, ...rest }) => {
+const Card = ({ noHover, title, pill, className, children, ...rest }) => {
   return (
     <div
-      className={classNames(`card px-3 py-4 lg:px-7 lg:py-6 rounded-md border-2 border-signature_border text-white bg-signature_light relative shadow-md
+      className={classNames(
+        `card px-3 py-4 lg:px-7 lg:py-6 rounded-md border-2 border-signature_border text-white bg-signature_light relative shadow-md
     transition duration-300 ${
       !noHover && "hover:border-signature_green hover:shadow-2xl"
-    } md:before:hover:!opacity-100 before:hover:!opacity-0`)}
+    } md:before:hover:!opacity-100 before:hover:!opacity-0`,
+        className
+      )}
+      {...rest}
     >
-      {title && <div className={`flex text-base text-xl w-full`}>{title}</div>}
+      {pill && title ? (
+        <div
+          className={`flex text-base text-xl w-full justify-between items-center`}
+        >
+          {title}
+          {pill}
+        </div>
+      ) : title ? (
+        <div className={`flex text-base text-xl w-full`}>{title}</div>
+      ) : pill ? (
+        <div className={`flex text-base text-xl w-full`}>{pill}</div>
+      ) : (
+        ""
+      )}
       <div className="h-full w-full flex">{children}</div>
     </div>
   );
