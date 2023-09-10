a7e1aa6c4eac4dfb885c56a79738caf37704ed20
Saurabh-stack <saurabh007ind@gmail.com>
2023-09-11 03:06:47 +0530

    DecisionInfo NewHireing Card

--- src/features/DecisionInfo/components/NewHieringDetails.jsx
+++ src/features/DecisionInfo/components/NewHieringDetails.jsx
@@ -6,6 +6,9 @@ import Pills from "../../../components/atoms/Pills.component";
 import PieChartActiveShape from "../../../components/ReCharts/PieChartActiveShape";
 import xhrClient from "../../../utilities/DataRequest";
 import { useQuery } from "@tanstack/react-query";
+import { createColumnHelper } from "@tanstack/react-table";
+import BaseTable from "../../../components/table/tanstackBaseTable/BaseTable";
+import WorkBasic from "./WorkBasic";
 const NewHieringDetails = () => {
   const [showTable, setShowTable] = useState(false);
   const [getTable, setTable] = useState(0);
@@ -38,10 +41,90 @@ const NewHieringDetails = () => {
     queryFn: () => xhrClient.get("buwiseempoccupancy").then((res) => res?.data),
   });
 
+  const columnHelper = createColumnHelper();
+  const columns = [
+    {
+      id: "expander",
+      header: () => null,
+      cell: ({ row }) => {
+        return row.getCanExpand() ? (
+          <button
+            {...{
+              onClick: row.getToggleExpandedHandler(),
+              style: { cursor: "pointer" },
+            }}
+          >
+            {row?.original?.status === "Consider" ? (
+              <Pills text={"Consider"} />
+            ) : row?.original?.status === "Reject" ? (
+              <Pills text={"Rejected"} />
+            ) : row.getIsExpanded() ? (
+              "👇"
+            ) : (
+              "👉"
+            )}
+          </button>
+        ) : (
+          "🔵"
+        );
+      },
+    },
+    columnHelper.accessor("Designation", {
+      cell: (info) => info.getValue(),
+      header: "Designation",
+    }),
+    columnHelper.accessor("totalExp", {
+      cell: (info) => info.getValue(),
+      header: "Total Experience",
+    }),
+    columnHelper.accessor("fullName", {
+      cell: (info) => info.getValue(),
+      header: "Name",
+    }),
+    columnHelper.accessor("profile", {
+      cell: (info) => info.getValue(),
+      header: "Profile",
+    }),
+    columnHelper.accessor("availableFrom", {
+      cell: (info) => info.getValue(),
+      header: "Available From",
+    }),
+  ];
+
+  const fetchData = async (options) => {
+    const data = await xhrClient.get("BenchEmployees").then((res) => res?.data);
+    console.log("data::", data);
+    return {
+      rows: data?.slice(
+        options.pageIndex * options.pageSize,
+        (options.pageIndex + 1) * options.pageSize
+      ),
+      pageCount: Math.ceil(30 / options.pageSize),
+    };
+  };
+
+  const renderSubComponent = (row) => {
+    return (
+      <WorkBasic
+        row={row}
+        considerHandler={() => {
+          alert("clicked");
+        }}
+        rejectHandler={() => {}}
+      />
+    );
+  };
+
   return (
     <CardContainer customClass={`border-0`}>
       <div className="grid grid-rows-2">
         <div className="grid grid-cols-4 gap-8 mt-5 mb-5">
+          <InfoCard
+            title={"Manager and Business Info"}
+            id={1}
+            description={<BusinessInfo />}
+            pill={<Pills text={"Critical"} />}
+          />
           <InfoCard
             className={"cursor-pointer"}
             title={"Bench Resources"}
@@ -63,30 +146,25 @@ const NewHieringDetails = () => {
             description={"15 bench resources with matching profile."}
             onClick={() => handleShowTable(3)}
           />
-          <InfoCard
-            title={"Manager and Business Info"}
-            id={1}
-            description={<BusinessInfo />}
-            pill={<Pills text={"Critical"} />}
-          />
         </div>
         <div className="grid gap-8 mt-5 mb-5">
           {showTable && (
-            <CardContainer>
-              <div className="grid grid-cols-4 mx-0 my-0">
-                <div className="grid col-span-3">
-                  1. Id 2. EmployeeName 3. Skills 4. Availabile(days) 5.
-                  Matching percentage 6. Actions (Download, Reject, Verify)
-                </div>
-                <div className="grid">
-                  <Card>
-                    <PieChartActiveShape
-                      data={buwiseempoccupancy.data[0].data}
-                    />
-                  </Card>
-                </div>
+            <div className="grid grid-cols-4 mx-0 my-0">
+              <div className="grid col-span-4">
+                1. Id 2. EmployeeName 3. Skills 4. Availabile(days) 5. Matching
+                percentage 6. Actions (Download, Reject, Verify)
+                <Card>
+                  <BaseTable
+                    columns={columns}
+                    fnFetchData={fetchData}
+                    renderSubComponent={renderSubComponent}
+                    getRowCanExpand={() => true}
+                    className={`border border-gray-700 text-left w-full`}
+                  />
+                  <PieChartActiveShape data={buwiseempoccupancy.data[0].data} />
+                </Card>
               </div>
-            </CardContainer>
+            </div>
           )}
         </div>
       </div>
